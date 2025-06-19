b44e7a5c9b7996222c2d79ce89b27206f07a40e2
Andy <andx667@protonmail.com>
2025-06-19 14:31:06 +0200

    add numbers

--- addons/tagging/ACE_Tags.hpp
+++ addons/tagging/ACE_Tags.hpp
@@ -1,4 +1,5 @@
 class ACE_Tags {
+    //Community Logos
     class GVAR(ttt_logo) {
         displayName = "TTT";
         requiredItem = QGVAR(tagging);
@@ -22,5 +23,68 @@ class ACE_Tags {
         requiredItem = QGVAR(tagging);
         textures[] = {QPATHTOEF(common,data\ui\prae_logo_ca.paa)};
         icon = QPATHTOEF(common,data\ui\prae_logo_ca.paa);
+    };
+
+    //Numbers
+    //Graphics by Gigls
+    class GVAR(number_zero) {
+        displayName = "0";
+        requiredItem = "ACE_SpraypaintBlack";
+        textures[] = {QPATHTOEF(common,data\ui\Zahl_0_ca.paa)};
+        icon = QPATHTOEF(common,data\ui\Zahl_0_ca.paa);
+    };
+    class GVAR(number_one) {
+        displayName = "1";
+        requiredItem = "ACE_SpraypaintBlack";
+        textures[] = {QPATHTOEF(common,data\ui\Zahl_1_ca.paa)};
+        icon = QPATHTOEF(common,data\ui\Zahl_1_ca.paa);
+    };
+    class GVAR(number_two) {
+        displayName = "2";
+        requiredItem = "ACE_SpraypaintBlack";
+        textures[] = {QPATHTOEF(common,data\ui\Zahl_2_ca.paa)};
+        icon = QPATHTOEF(common,data\ui\Zahl_2_ca.paa);
+    };
+    class GVAR(number_three) {
+        displayName = "3";
+        requiredItem = "ACE_SpraypaintBlack";
+        textures[] = {QPATHTOEF(common,data\ui\Zahl_3_ca.paa)};
+        icon = QPATHTOEF(common,data\ui\Zahl_3_ca.paa);
+    };
+    class GVAR(number_four) {
+        displayName = "4";
+        requiredItem = "ACE_SpraypaintBlack";
+        textures[] = {QPATHTOEF(common,data\ui\Zahl_4_ca.paa)};
+        icon = QPATHTOEF(common,data\ui\Zahl_4_ca.paa);
+    };
+    class GVAR(number_five) {
+        displayName = "5";
+        requiredItem = "ACE_SpraypaintBlack";
+        textures[] = {QPATHTOEF(common,data\ui\Zahl_5_ca.paa)};
+        icon = QPATHTOEF(common,data\ui\Zahl_5_ca.paa);
+    };
+    class GVAR(number_six) {
+        displayName = "6";
+        requiredItem = "ACE_SpraypaintBlack";
+        textures[] = {QPATHTOEF(common,data\ui\Zahl_6_ca.paa)};
+        icon = QPATHTOEF(common,data\ui\Zahl_6_ca.paa);
+    };
+    class GVAR(number_seven) {
+        displayName = "7";
+        requiredItem = "ACE_SpraypaintBlack";
+        textures[] = {QPATHTOEF(common,data\ui\Zahl_7_ca.paa)};
+        icon = QPATHTOEF(common,data\ui\Zahl_7_ca.paa);
+    };
+    class GVAR(number_eight) {
+        displayName = "8";
+        requiredItem = "ACE_SpraypaintBlack";
+        textures[] = {QPATHTOEF(common,data\ui\Zahl_8_ca.paa)};
+        icon = QPATHTOEF(common,data\ui\Zahl_8_ca.paa);
+    };
+    class GVAR(number_nine) {
+        displayName = "9";
+        requiredItem = "ACE_SpraypaintBlack";
+        textures[] = {QPATHTOEF(common,data\ui\Zahl_9_ca.paa)};
+        icon = QPATHTOEF(common,data\ui\Zahl_9_ca.paa);
     };
 };
